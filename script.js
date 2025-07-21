e19dbd0d0cf8198c6276969c81dece6a60206ca8
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-21 20:03:53 +0100

    Add change width button functionality

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 const grid = document.querySelector("#grid");
+let percentage = 70;
 let size = 16;
-createGrid(size);
-
-const changeSizeBtn = document.querySelector(".change-resolution");
-changeSizeBtn.addEventListener("click", () => {
-  size = getUserInputNumber(1, 100, "number");
-  if (size === null) return;
-  createGrid(size);
-});
 
 window.addEventListener("resize", () => { 
   const allSquares = document.querySelectorAll(".square");
@@ -17,13 +10,31 @@ window.addEventListener("resize", () => {
     square.style.height = newWidth;
   })
 });
+
+createGrid(size);
+setGridWidth();
+
+const changeSizeBtn = document.querySelector(".change-resolution");
+changeSizeBtn.addEventListener("click", () => {
+  size = getUserInputNumber(1, 100, "number");
+  if (size === null) return;
+  createGrid(size);
+});
+
+
+const changeWidthBtn = document.querySelector(".change-width");
+changeWidthBtn.addEventListener("click", () => {
+  percentage = getUserInputNumber(1, 100, "pecentage");
+  setGridWidth();
+});
+
+
 function getUserInputNumber(low, high, display) {
   let num = NaN;
   while (num > high || num < low || isNaN(num)) {
     num = prompt(`Enter a ${display} between ${low} and ${high}.`);
     if (num === null || num === "") return null;
     num = +num;
-    console.log(num, typeof num);
   };
   return num;
 };
@@ -45,3 +56,8 @@ function createGrid(size) {
     grid.appendChild(row);
   };
 };
+
+function setGridWidth() {
+  grid.style.width =`${percentage}vw`;
+  window.dispatchEvent(new Event("resize"));
+}
