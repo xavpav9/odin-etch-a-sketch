5c8855b43d0523ab513faac5002b9cd6c5c78681
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-21 17:41:50 +0100

    Add hover effect

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ for (let o = 0; o < 16; ++o) {
   for (let i = 0; i < 16; ++i) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
+    square.addEventListener("mouseenter", evt => evt.target.style.backgroundColor = "black");
     row.appendChild(square);
   };
   grid.appendChild(row);
 };
-    
+
