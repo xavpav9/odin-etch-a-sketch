562377d7926fb073c2365cfca60764b04f3a177f
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-21 18:06:27 +0100

    Add change size button and functionality

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const grid = document.querySelector("#grid");
-for (let o = 0; o < 16; ++o) {
-  const row = document.createElement("div");
-  row.setAttribute("class", "row");
-  for (let i = 0; i < 16; ++i) {
-    const square = document.createElement("div");
-    square.setAttribute("class", "square");
-    square.addEventListener("mouseenter", evt => evt.target.style.backgroundColor = "black");
-    row.appendChild(square);
-  };
-  grid.appendChild(row);
-};
+createGrid(16);
 
+const changeSizeBtn = document.querySelector(".change-size");
+changeSizeBtn.addEventListener("click", () => {
+  let newSize = prompt("Enter a new size, between 1 and 100");
+  while (newSize > 100 || newSize < 0) {
+    newSize = prompt("Enter a new size, between 1 and 100");
+  };
+  createGrid(newSize);
+});
+
+
+function createGrid(size) {
+  while (grid.firstElementChild) grid.firstElementChild.remove();
+  const newWidth = (100 / size).toString() + "vw";
+  console.log(newWidth);
+  for (let o = 0; o < size; ++o) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let i = 0; i < size; ++i) {
+      const square = document.createElement("div");
+      square.setAttribute("class", "square");
+      square.style.width = newWidth;
+      square.style.height = newWidth;
+      square.addEventListener("mouseenter", evt => evt.target.style.backgroundColor = "black");
+      row.appendChild(square);
+    };
+    grid.appendChild(row);
+  };
+};
